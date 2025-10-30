8e641a9793c02ee504547a68af015e81e0ca0cef
Athulms7 <22ct528@mgits.ac.in>
2025-10-30 16:40:42 +0530

    pkg changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
-import Image from "next/image";
-import { Header } from "./_components/Appbar";
+import Navbar from "./_components/Navbar";
+import Hero from "./_components/Hero";
+import Features from "./_components/Features";
+import Streamers from "./_components/Streamers";
+import Pricing from "./_components/Pricing";
+import Footer from "./_components/Footer";
 
-export default function Home() {
+const Index = () => {
   return (
-    <div>
-     <Header/>
+    <div className="min-h-screen">
+      <Navbar />
+      <Hero />
+      <Features />
+      <Streamers />
+      <Pricing />
+      <Footer />
     </div>
   );
-}
+};
+
+export default Index;
